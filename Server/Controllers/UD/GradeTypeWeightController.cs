c4ba6034d2f3786a015b143985f7aa5ea0e529f7
aparnaroy <aparnar@udel.edu>
2023-11-13 19:47:46 -0500

    Finished GradeTypeWeightController

---
+++ Server/Controllers/UD/GradeTypeWeightController.cs
@@ -0,0 +1,227 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+//using OCTOBER.EF.Data;
+using OCTOBER.EF.Models;
+using OCTOBER.Shared;
+using Telerik.DataSource;
+using Telerik.DataSource.Extensions;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System.Linq.Dynamic.Core;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.CodeAnalysis;
+using AutoMapper;
+using OCTOBER.Server.Controllers.Base;
+using OCTOBER.Shared.DTO;
+using OCTOBER.EF;
+using static System.Collections.Specialized.BitVector32;
+
+namespace OCTOBER.Server.Controllers.UD
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class GradeTypeWeightController : BaseController, GenericRestController<GradeTypeWeightDTO>
+    {
+        // Constructor
+        public GradeTypeWeightController(OCTOBEROracleContext context,
+            IHttpContextAccessor httpContextAccessor,
+            IMemoryCache memoryCache)
+        : base(context, httpContextAccessor)
+        {
+        }
+
+
+        [HttpGet]
+        [Route("Get")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var result = await _context.GradeTypeWeights.Select(sp => new GradeTypeWeightDTO
+                {
+                    CreatedBy = sp.CreatedBy,
+                    CreatedDate = sp.CreatedDate,
+                    DropLowest = sp.DropLowest,
+                    GradeTypeCode = sp.GradeTypeCode,
+                    ModifiedBy = sp.ModifiedBy,
+                    ModifiedDate = sp.ModifiedDate,
+                    NumberPerSection = sp.NumberPerSection,
+                    PercentOfFinalGrade = sp.PercentOfFinalGrade,
+                    SchoolId = sp.SchoolId,
+                    SectionId = sp.SectionId
+                })
+                .ToListAsync();
+                await _context.Database.RollbackTransactionAsync();
+                return Ok(result);
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+
+        [HttpGet]
+        [Route("Get/{SchoolID}/{SectionID}/{GradeTypeCode}")]
+        public async Task<IActionResult> Get(int SchoolID, int SectionID, string GradeTypeCode)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                GradeTypeWeightDTO? result = await _context.GradeTypeWeights
+                    .Where(x => x.SchoolId == SchoolID)
+                    .Where(x => x.SectionId == SectionID)
+                    .Where(x => x.GradeTypeCode.Equals(GradeTypeCode))
+                    .Select(sp => new GradeTypeWeightDTO
+                    {
+                        CreatedBy = sp.CreatedBy,
+                        CreatedDate = sp.CreatedDate,
+                        DropLowest = sp.DropLowest,
+                        GradeTypeCode = sp.GradeTypeCode,
+                        ModifiedBy = sp.ModifiedBy,
+                        ModifiedDate = sp.ModifiedDate,
+                        NumberPerSection = sp.NumberPerSection,
+                        PercentOfFinalGrade = sp.PercentOfFinalGrade,
+                        SchoolId = sp.SchoolId,
+                        SectionId = sp.SectionId
+                    })
+                .SingleOrDefaultAsync();
+                await _context.Database.RollbackTransactionAsync();
+                return Ok(result);
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+
+        [HttpPost]
+        [Route("Post")]
+        public async Task<IActionResult> Post([FromBody] GradeTypeWeightDTO _GradeTypeWeightDTO)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var itm = await _context.GradeTypeWeights
+                    .Where(x => x.SchoolId == _GradeTypeWeightDTO.SchoolId)
+                    .Where(x => x.SectionId == _GradeTypeWeightDTO.SectionId)
+                    .Where(x => x.GradeTypeCode.Equals(_GradeTypeWeightDTO.GradeTypeCode))
+                    .FirstOrDefaultAsync();
+
+                if (itm == null)
+                {
+                    GradeTypeWeight g = new GradeTypeWeight
+                    {
+                        // All columns w/o createds and modifieds
+                        DropLowest = _GradeTypeWeightDTO.DropLowest,
+                        GradeTypeCode = _GradeTypeWeightDTO.GradeTypeCode,
+                        NumberPerSection = _GradeTypeWeightDTO.NumberPerSection,
+                        PercentOfFinalGrade = _GradeTypeWeightDTO.PercentOfFinalGrade,
+                        SchoolId = _GradeTypeWeightDTO.SchoolId,
+                        SectionId = _GradeTypeWeightDTO.SectionId
+                    };
+                    _context.GradeTypeWeights.Add(g);
+                    await _context.SaveChangesAsync();
+                    await _context.Database.CommitTransactionAsync();
+                }
+                return Ok();
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+
+        [HttpPut]
+        [Route("Put")]
+        public async Task<IActionResult> Put([FromBody] GradeTypeWeightDTO _GradeTypeWeightDTO)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var itm = await _context.GradeTypeWeights
+                    .Where(x => x.SchoolId == _GradeTypeWeightDTO.SchoolId)
+                    .Where(x => x.SectionId == _GradeTypeWeightDTO.SectionId)
+                    .Where(x => x.GradeTypeCode.Equals(_GradeTypeWeightDTO.GradeTypeCode))
+                    .FirstOrDefaultAsync();
+
+                // All columns w/o PKs, createds, and modifieds
+                itm.DropLowest = _GradeTypeWeightDTO.DropLowest;
+                itm.NumberPerSection = _GradeTypeWeightDTO.NumberPerSection;
+                itm.PercentOfFinalGrade = _GradeTypeWeightDTO.PercentOfFinalGrade;
+
+                _context.GradeTypeWeights.Update(itm);
+                await _context.SaveChangesAsync();
+                await _context.Database.CommitTransactionAsync();
+
+                return Ok();
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+
+        [HttpDelete]
+        [Route("Delete/{SchoolID}/{SectionID}/{GradeTypeCode}")]
+        public async Task<IActionResult> Delete(int SchoolID, int SectionID, string GradeTypeCode)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var itm = await _context.GradeTypeWeights
+                    .Where(x => x.SchoolId == SchoolID)
+                    .Where(x => x.SectionId == SectionID)
+                    .Where(x => x.GradeTypeCode.Equals(GradeTypeCode))
+                    .FirstOrDefaultAsync();
+
+                if (itm != null)
+                {
+                    _context.GradeTypeWeights.Remove(itm);
+                }
+                await _context.SaveChangesAsync();
+                await _context.Database.CommitTransactionAsync();
+
+                return Ok();
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+
+        public Task<IActionResult> Get(int KeyVal)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<IActionResult> Delete(int KeyVal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Shared/DTO/GradeTypeWeightDTO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OCTOBER.Shared.DTO
+{
+	public class GradeTypeWeightDTO
+	{
+        [Precision(8)]
+        public int SchoolId { get; set; }
+        [Key]
+        [Column("SECTION_ID")]
+        [Precision(8)]
+        public int SectionId { get; set; }
+        [Key]
+        [Column("GRADE_TYPE_CODE")]
+        [StringLength(2)]
+        [Unicode(false)]
+        public string GradeTypeCode { get; set; } = null!;
+        [Column("NUMBER_PER_SECTION")]
+        [Precision(3)]
+        public byte NumberPerSection { get; set; }
+        [Column("PERCENT_OF_FINAL_GRADE")]
+        [Precision(3)]
+        public byte PercentOfFinalGrade { get; set; }
+        [Column("DROP_LOWEST")]
+        [Precision(1)]
+        public bool DropLowest { get; set; }
+        [Column("CREATED_BY")]
+        [StringLength(30)]
+        [Unicode(false)]
+        public string CreatedBy { get; set; } = null!;
+        [Column("CREATED_DATE", TypeName = "DATE")]
+        public DateTime CreatedDate { get; set; }
+        [Column("MODIFIED_BY")]
+        [StringLength(30)]
+        [Unicode(false)]
+        public string ModifiedBy { get; set; } = null!;
+        [Column("MODIFIED_DATE", TypeName = "DATE")]
+        public DateTime ModifiedDate { get; set; }
+    }
+}
+
